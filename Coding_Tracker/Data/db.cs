6da67bbce5411ef023bd574f21b945270ac549f9
Stuart Willows <SJWillows91@outlook.com>
2022-11-07 21:24:04 +0000

    Added SQlite package and starting CreateDatabase method.

---
+++ Coding_Tracker/Data/db.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Data.Sqlite;
+
+namespace Coding_Tracker.Data;
+internal class db
+{
+    public static string ConnectionString
+    {
+        get
+        {
+            return System.Configuration.ConfigurationManager.ConnectionStrings["Coding_Tracker.Properties.Settings.CodingTrackerConnectionString"].ConnectionString;
+        }
+    }
+
+    public static void CreateDatabase()
+    {
+        using var connection = new SqliteConnection(ConnectionString);
+        using var command = connection.CreateCommand();
+
+        connection.Open();
+
+        command.CommandText = @"CREATE TABLE IF NOT EXISTS CodingTracker 
+                                (Id INTEGER PRIMARY KEY AUTOINCREMENT, 
+                                 Date TEXT, 
+                                 StartTime TEXT, 
+                                 FinishTime TEXT,
+                                 Duration TEXT
+)";
+
+        try
+        {
+            command.ExecuteNonQuery();
+        }
+        catch ( Exception e )
+        {
+            Console.WriteLine.de ;
+        }
+    }
+    }
+}
