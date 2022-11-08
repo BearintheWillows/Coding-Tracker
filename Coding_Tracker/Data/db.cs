431b897591570403da4e4d7759683517d52c29a3
Stuart Willows <SJWillows91@outlook.com>
2022-11-08 22:03:53 +0000

    Added db.Add method

--- Coding_Tracker/Data/db.cs
+++ Coding_Tracker/Data/db.cs
@@ -45,6 +45,37 @@ internal class Db
         }
     }
 
+    public void Add(DateTime date,
+                    DateTime startTime, 
+                    DateTime finishTime 
+                   )
+    {
+        TimeSpan duration = finishTime - startTime;
 
+        using var connection = new SqliteConnection(ConnectionString);
+
+        using var command = connection.CreateCommand();
+
+        
+        connection.Open();
+
+        command.CommandText = $@"INSERT INTO CodingTracker(date, startTime, finishTime, duration) VALUES (@date, @startTime, @finishTime, @duration)";
+
+        command.Parameters.AddWithValue( "@date", date );
+        command.Parameters.AddWithValue( "@startTime", startTime );
+        command.Parameters.AddWithValue( "@finishTime", finishTime );
+        command.Parameters.AddWithValue( "@duration", duration );
+
+        try
+        {
+            command.ExecuteNonQuery();
+            Log.Information( "Data Inserted Successfully" );
+        }
+        catch ( Exception e)
+        {
+            Log.Warning( "Data not Inserted" );
+            Log.Debug( e.Message );
+        }
+    }
 }
 
