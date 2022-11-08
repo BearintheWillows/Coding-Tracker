b23b754b13f17e6641459f2358d4311b59252e23
Stuart Willows <SJWillows91@outlook.com>
2022-11-08 21:48:12 +0000

    Added CodeSession Model

---
+++ Coding_Tracker/Models/CodingSession.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coding_Tracker.Models;
+internal class CodingSession
+{
+    public int Id { get; set; }
+    public DateTime Date { get; set; }
+    public DateTime StartTime { get; set; }
+    public DateTime FinishTime { get; set; }
+    public TimeSpan Duration { get; set; }
+}
